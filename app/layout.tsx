038eea1cef6fce25ea676c9ccafadb418de5a4d8
menadeth <bernadeth@swrtech.io>
2024-02-10 16:21:33 +0800

    import clsx, tailwind-merge, daisyUI and zod

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "./utils/cn";
+import { publicSans } from "./constant/font";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(publicSans, 'indeterminate h-screen bg-white')}>{children}</body>
     </html>
   );
 }
