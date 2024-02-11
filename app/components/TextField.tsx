f4b240483539e204fcf23ac7f2dd99dfc5632af1
Bernadeth <106055637+menadeth@users.noreply.github.com>
2024-02-11 11:04:38 +0800

    feat:inputfield component (#1)
    * remove zod library
    
    * feat: update TailwindCSS config with custom themes and DaisyUI plugin
    
    * feat: add utility function to merge TailwindCSS classes with clsx and tailwind-merge
    
    * feat:create Textfield component
    
    * refactor: standardize import of cn utility function across components

---
+++ app/components/TextField.tsx
@@ -0,0 +1,46 @@
+import { InputHTMLAttributes, forwardRef } from "react";
+import { cn } from "@/app/utils/cn";
+
+type TextFieldProps = Omit<
+  InputHTMLAttributes<HTMLInputElement>,
+  "className"
+> & {
+  placeholder: string;
+  className?: string;
+  hasValue?: boolean;
+};
+
+export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
+  function Comp(
+    { placeholder, className, hasValue = false, ...inputParams },
+    ref
+  ) {
+    return (
+      <div className="flex flex-col relative">
+        <input
+          ref={ref}
+          {...inputParams}
+          placeholder={placeholder}
+          className={cn(
+            "px-3 py-2 bg-white border shadow-sm border-primary/25 placeholder:text-primary/25 placeholder-slate-400 focus:outline-none focus:border-primary focus:ring-0 block w-full rounded-md sm:text-sm peer placeholder:font-medium placeholder:text-xs",
+            {
+              "border-primary text-primary ring-0 outline-none": !hasValue,
+            }
+          )}
+        />
+        {placeholder && (
+          <p
+            className={cn(
+              "absolute -top-3.5 left-2 w-fit invisible peer-focus:visible text-primary text-xs bg-white p-1 font-medium",
+              {
+                visible: !hasValue,
+              }
+            )}
+          >
+            {placeholder}
+          </p>
+        )}
+      </div>
+    );
+  }
+);
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#3D3D3D",
+        secondary: "#E01A22",
       },
     },
   },
